02b0eebae3637471a81e4fce3b3af45f100b11c4
Molly Cooper <mollycooper64@gmail.com>
2020-05-08 20:15:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,11 +4,7 @@ def valid_move?(board, index)
 #returns true for a valid position on an empty board
 #board[index] == be_between?(0,8) && !position_taken?(board, index)
 
-if !position_taken?(board, index)
-  board[index].between?(0,8) == true
-else
-  false
-end
+index.between?(0,8) && !position_taken
 
 end
 
